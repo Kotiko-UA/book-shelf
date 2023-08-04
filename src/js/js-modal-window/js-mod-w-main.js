ac4300e7a2e17fec04eaa1cd608f0eb161cb1cb4
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-08-04 23:10:13 +0300

    id

--- src/js/js-modal-window/js-mod-w-main.js
+++ src/js/js-modal-window/js-mod-w-main.js
@@ -1,30 +1,38 @@
 import axios from 'axios';
 
+const elements = {
+    categoryList: document.querySelector(".js-category-list"),
+    books_showcase: document.querySelector(".books-showcase")
+}
+
 elements.books_showcase.addEventListener('click', handlerClickOpenModal);
 
 function handlerClickOpenModal(evt) {
   const bookItem = evt.target.closest('.js-book-item'); //js-book-item  - клас для li
-  if (!bookItem) {
-    // або (evt.target === evt.currentarget)
+  if (!bookItem) {     // або (evt.target === evt.currentarget)
     return;
   } else {
-    serviceBooks()
-      .then(response => {
-        bookModalMarkup(response.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    const { id } = bookItem.dataset;
+    console.log(id);
+      serviceBooks()
+        .then(response => {
+      console.log(response);
+          bookModalMarkup(response.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   }
+
+
+async function serviceBooks(id) {
+    const BASE_URL = 'https://books-backend.p.goit.global/books';
+    const response = await axios.get(`${BASE_URL}${id}`);
+    console.log(response);
+    return response;
 }
 
-async function serviceBooks() {
-  const BASE_URL = 'https://books-backend.p.goit.global/books/';
-  const id = '643282b1e85766588626a0dc';
-  const response = await axios.get(`${BASE_URL}${id}`);
-  //   console.log(response);
-  return response;
-}
 
 function bookModalMarkup({ book_image, list_name, author, description }) {
   const arr = `<div>
@@ -38,10 +46,13 @@ function bookModalMarkup({ book_image, list_name, author, description }) {
   return arr;
 }
 
+
+
 // async function serviceBooks() {
-//     const BASE_URL = 'https://books-backend.p.goit.global/books';
-//     const { id } = bookItem.dataset; // або evt.target.dataset
-//     const response = await axios.get(`${BASE_URL}${id}`);
-//     console.log(response);
-//     return response;
+//   const BASE_URL = 'https://books-backend.p.goit.global/books/';
+//   const id = '643282b1e85766588626a0dc';
+//   const response = await axios.get(`${BASE_URL}${id}`);
+//   //   console.log(response);
+//   return response;
 // }
+
