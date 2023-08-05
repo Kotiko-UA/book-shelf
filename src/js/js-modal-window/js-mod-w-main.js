5fa35fe8f95994ef82630cc7210ed007dababb6d
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-08-06 01:25:53 +0300

    local-storage-try1

--- src/js/js-modal-window/js-mod-w-main.js
+++ src/js/js-modal-window/js-mod-w-main.js
@@ -21,6 +21,10 @@ async function handlerClickOpenModal(evt) {
       const bookData = await serviceBooks(id);
       const modalMarkup = bookModalMarkup(bookData);
       openModalWithContent(modalMarkup);
+       const modalWindow = document.querySelector('.modal-window-conteiner');
+      const text = modalWindow.querySelector('.added-modal-window');
+      text.classList.add('after');
+      modalWindow.addEventListener('click', handlerAddToBascet);
     } catch (error) {
       console.error(error);
     }
@@ -42,8 +46,8 @@ async function serviceBooks(id) {
 
 function bookModalMarkup({ _id, book_image, list_name, author, description, buy_links: [amazon, Bookshop, Apple, Barnes, IndieBound]} = {}) {
   const markup = `
-  <div class = "modal-window-conteiner">
-    <div data-id=${_id} class = "mw-content-conteiner">
+  <div data-id=${_id} class = "modal-window-conteiner">
+    <div  class = "mw-content-conteiner">
       <div class ="mw-image-conteiner">
         <img class = "book-img-modal-window" src="${book_image}" alt="" />
       </div>
@@ -70,40 +74,30 @@ function bookModalMarkup({ _id, book_image, list_name, author, description, buy_
       </ul>
       </div>
     </div>
-    <button class = "add-btn-modal-window">ADD TO SHOPPING LIST</button>
+    <button class = "add-delete-book">ADD TO SHOPPING LIST</button>
+    <p class="added-modal-window">
+      Сongratulations! You have added the book to the shopping list. To delete,
+      press the button “Remove from the shopping list”.
+    </p>
   </div>
  `;
   return markup;
 }
 
-// const div = document.querySelector('.shopping-list');
-// div.innerHTML = `
-//   <div data-id="${_id}" class="mw-content-conteiner">
-//     <div class="for">
-//     <img class="book-img-modal-window" src="${book_image}" alt="" /></div>
-//     <div>
-//     <button class="btn">ADD TO SHOPPING LIST</button>
-//     <p class="added">
-//   Сongratulations! You have added the book to the shopping list. To delete,
-//   press the button “Remove from the shopping list”.
-// </p>
-//       <h2 class="book-title-modal">${list_name}</h2>
-//       <p class="author-modal-window">${author}</p>
-//       <p class="desc-modal-window">${description}</p>
-//       <ul class= " list market list">
-//         <li  class="">
-//           <a href="${amazon.url}" class="link">
-//             <img src="/img/amazon.png" alt="${list_name} heigth= 250></a>
-//         </li>
-//         <li class="">
-//           <a href="${Bookshop.url}" class="link">
-//             <img src="/img/book-market.png" alt="${list_name}</a>
-//         </li>
-//         <li class="">
-//           <a href="${Apple.url}" class="link">
-//             <img src="/img/book-shelf.png" alt="${list_name}</a>
-//         </li>
-//       </ul>
-//     </div>
-//    </div>
-// `;
+function handlerAddToBascet(event) {
+  if (!event.target.classList.contains('add-delete-book')) {
+    return;
+  }
+  console.log(event.target);
+  const arrForBAcket = JSON.parse(localStorage.getItem('KEY')) ?? [];
+  const { id } = event.target.closest('.modal-window-conteiner').dataset;
+  console.log(id);
+  if (_id === id) {
+    arrForBAcket.push(arr);
+    localStorage.setItem('KEY', JSON.stringify(arrForBAcket));
+    console.log(JSON.parse(localStorage.getItem('KEY')));
+    text.classList.remove('after');
+    btn.textContent = 'Remove from the shopping list';
+    modalWindow.removeEventListener('click', handlerAddToBascet);
+  }
+}
