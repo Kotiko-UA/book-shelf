da3014408b7ea82d833549470e65c448dab35656
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-08-06 00:05:06 +0300

    markets-img

--- src/js/js-modal-window/js-mod-w-main.js
+++ src/js/js-modal-window/js-mod-w-main.js
@@ -39,10 +39,11 @@ async function serviceBooks(id) {
   return response.data; // Повертайте тільки дані з response, а не увесь об'єкт response
 }
 
-function bookModalMarkup({ book_image, list_name, author, description} = {}) {
+
+function bookModalMarkup({ _id, book_image, list_name, author, description, buy_links: [amazon, Bookshop, Apple, Barnes, IndieBound]} = {}) {
   const markup = `
   <div class = "modal-window-conteiner">
-    <div class = "mw-content-conteiner">
+    <div data-id=${_id} class = "mw-content-conteiner">
       <div class ="mw-image-conteiner">
         <img class = "book-img-modal-window" src="${book_image}" alt="" />
       </div>
@@ -50,6 +51,23 @@ function bookModalMarkup({ book_image, list_name, author, description} = {}) {
         <h2 class = "book-title-modal">${list_name}</h2>
         <p class = "author-modal-window">${author}</p>
         <p class = "desc-modal-window">${description}</p>
+        <ul class= "markets-list-modal-window">
+        <li  class="">
+          <a href="${amazon.url}" class="">
+            <img src="/img/amazon.png" alt="${list_name}">
+          </a>
+        </li>
+        <li class="">
+          <a href="${Bookshop.url}" class="">
+            <img src="/img/book-market.png" alt="${list_name}">
+          </a>
+        </li>
+        <li class="">
+          <a href="${Apple.url}" class="">
+            <img src="/img/book-shelf.png" alt="${list_name}">
+          </a>
+        </li>
+      </ul>
       </div>
     </div>
     <button class = "add-btn-modal-window">ADD TO SHOPPING LIST</button>
@@ -57,3 +75,35 @@ function bookModalMarkup({ book_image, list_name, author, description} = {}) {
  `;
   return markup;
 }
+
+// const div = document.querySelector('.shopping-list');
+// div.innerHTML = `
+//   <div data-id="${_id}" class="mw-content-conteiner">
+//     <div class="for">
+//     <img class="book-img-modal-window" src="${book_image}" alt="" /></div>
+//     <div>
+//     <button class="btn">ADD TO SHOPPING LIST</button>
+//     <p class="added">
+//   Сongratulations! You have added the book to the shopping list. To delete,
+//   press the button “Remove from the shopping list”.
+// </p>
+//       <h2 class="book-title-modal">${list_name}</h2>
+//       <p class="author-modal-window">${author}</p>
+//       <p class="desc-modal-window">${description}</p>
+//       <ul class= " list market list">
+//         <li  class="">
+//           <a href="${amazon.url}" class="link">
+//             <img src="/img/amazon.png" alt="${list_name} heigth= 250></a>
+//         </li>
+//         <li class="">
+//           <a href="${Bookshop.url}" class="link">
+//             <img src="/img/book-market.png" alt="${list_name}</a>
+//         </li>
+//         <li class="">
+//           <a href="${Apple.url}" class="link">
+//             <img src="/img/book-shelf.png" alt="${list_name}</a>
+//         </li>
+//       </ul>
+//     </div>
+//    </div>
+// `;
