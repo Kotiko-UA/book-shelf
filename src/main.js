5ea6d4fe666c16fec415fd3b6655001c1408eae2
OleksandrSHAR <125485600+OleksandrSHAR@users.noreply.github.com>
2023-08-06 14:45:53 +0300

    1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import './js/js-modal-window/js-mod-w-main';
 import './js/category-js/main_js_category';
 import './js/shoping-js/main-shopping-list';
-import './js/heder-js/theme'
+import './js/heder-js/theme';
