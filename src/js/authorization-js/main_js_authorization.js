45ebd496c324123ea30d7e4441a1cba9d9063818
lusya0409 <lusya0409@gmail.com>
2023-08-06 17:37:08 +0300

    changes auth.js

--- src/js/authorization-js/main_js_authorization.js
+++ src/js/authorization-js/main_js_authorization.js
@@ -1,113 +1,31 @@
-import * as basicLightbox from 'basiclightbox';
+import { signUpModal, signInModal } from './auth_modals';
+import { logOutUser, signIn, registrateUser } from './auth_firebase';
 
-const openModalBtn = document.querySelector('.modal-open-btn');
-
-openModalBtn.addEventListener('click', openModalAuth);
-
-function openModalAuth() {
-  instance.show();
-}
+//===========================================================
 
 document.addEventListener('click', clickHandlerAuth);
 
 function clickHandlerAuth(e) {
   if (e.target.closest('.modal-close-btn')) {
-    instance.close();
+    signUpModal.close();
+    signInModal.close();
+  }
+  if (e.target.closest('[data-sign-up]')) {
+    signUpModal.show();
+    signInModal.close();
+  }
+  if (e.target.closest('[data-sign-in]')) {
+    signUpModal.close();
+    signInModal.show();
+  }
+  if (e.target.closest('.log-out-btn')) {
+    logOutUser();
+  }
+  if (e.target.closest('.sign-in-btn')) {
+    signIn();
+  }
+  if (e.target.closest('.sign-up-btn')) {
+    registrateUser();
   }
 }
-
-const instance = basicLightbox.create(
-  `<div class="modal">
-    <button type="button" class="modal-close-btn">
-      <svg class="icon-close" width="28" height="28">
-        <use href="img/spryte.svg#icon-close"></use>
-      </svg>
-    </button>
-
-    <div class="form-wraper">
-      <form class="form" action="submit" autocomplete="off">
-        <div class="form-field">
-          <input
-            id="name"
-            class="form-input"
-            type="text"
-            name="user-name"
-            minlength="2"
-            required
-            autofocus
-            placeholder=" "
-            title="Djon Smit"
-          />
-
-          <label for="name" class="form-label">Name* </label>
-        </div>
-
-        <div class="form-field">
-          <input
-            id="email"
-            class="form-input"
-            type="email"
-            name="email"
-            required
-            placeholder=" "
-          />
-
-          <label for="email" class="form-label">Email* </label>
-          <svg class="icon" width="28" height="28">
-            <use href="img/spryte.svg#icon-mail"></use>
-          </svg>
-
-        </div>
-        <div class="form-field">
-          <input
-            id="password"
-            class="form-input"
-            type="password"
-            name="password"
-            required
-            placeholder=" "
-            minlength="6"
-          />
-
-          <label for="password" class="form-label">Password* </label>
-          <svg class="icon" width="28" height="28">
-            <use href="img/spryte.svg#icon-lock"></use>
-          </svg>
-
-        </div>
-
-        <button class="form-button">Sign up</button>
-      </form>
-    </div>
-  </div>
-`,
-  {
-    onShow: () => document.addEventListener('keydown', onEscKeyPress),
-    onClose: () => document.removeEventListener('keydown', onEscKeyPress),
-  }
-);
-
-function onEscKeyPress(evt) {
-  console.log(evt.code);
-  const ESC_KEY_CODE = 'Escape';
-  if (evt.code === ESC_KEY_CODE) {
-    instance.close();
-  }
-}
-//========================================
-// const form = document.querySelector('.js-register-form');
-
-// form.addEventListener('submit', onFormSubmit);
-
-// function onFormSubmit(event) {
-//   event.preventDefault();
-
-//   const formData = new FormData(event.currentTarget);
-
-//   console.log(formData);
-
-//   formData.forEach((value, name) => {
-//     console.log('onFormSubmit -> name', name);
-//     console.log('onFormSubmit -> value', value);
-//   });
-// }
+document.querySelector('body').classList.add('dark-theme');
