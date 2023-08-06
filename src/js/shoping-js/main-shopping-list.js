6ee74d5489f8ac3b21367c3c31b7b2a69b175708
OleksandrSHAR <125485600+OleksandrSHAR@users.noreply.github.com>
2023-08-06 14:31:01 +0300

    Squashed commit of the following:
    commit 8e1cf323a3b11b0df8130c9c88157dc4d7efa06f
    Merge: 17b448e 5195aa6
    Author: Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
    Date:   Sun Aug 6 13:21:57 2023 +0300
    
        Merge pull request #13 from Kotiko-UA/shopping-section
    
        add-get-local-books
    
    commit 5195aa62dec196f1858f436f37a9a1c45b300f5c
    Author: VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
    Date:   Sun Aug 6 00:01:57 2023 +0200
    
        add-get-local-books

--- src/js/shoping-js/main-shopping-list.js
+++ src/js/shoping-js/main-shopping-list.js
@@ -1,28 +1,47 @@
-import Pagination from 'tui-pagination';
-import 'tui-pagination/dist/tui-pagination.css';
+// import Pagination from 'tui-pagination';
+// import 'tui-pagination/dist/tui-pagination.css';
 
+
+// const BOOK_ID_INFO = 'book-id-info' - взяти від Ані
 const bookList = document.querySelector('.shopping-list');
 
+
+
+
+  const choosedBooksArray = JSON.parse(localStorage.getItem(BOOK_ID_INFO)) || [];
+  console.log("choosedBooks", choosedBooks)
+  //прилітає масив доданих у кошик книг
+
+
+  choosedBooksArray.map(book => {
+  bookList.insertAdjacentHTML('beforeend', generateMovieHTML(book));
+});
+
+
+
 function generateMovieHTML(bookData) {
-  const { image, title, genre, description, author } = bookData;
+  const { book_image, list_name, author, description } = bookData;
   return `
   <li class="shopping-list-item">
   <img
-    src="${image}"
+    src="${book_image}"
     alt="book's image"
     class="shopping-card-img"
   />
   <div class="right-part-wrap">
     <div class="shopping-card-bin-wrap">
      <div>
-        <h4 class="shopping-card-tittle">${title}</h4>
+        <h4 class="shopping-card-tittle">${list_name}</h4>
 
-        <p class="shopping-card-genre">${genre}</p>
+        <p class="shopping-card-genre">${book_id_genre}</p>
      </div>
       <a href="" class="shopping-card-bin-link">
-        <svg class="img-bin-icon">
-          <use href="../img/spryte.svg#icon-trash"></use>
-        </svg>
+        <button type="button" class ="btn-png-bin">
+          <svg class="img-bin-icon">
+            <use href="../img/spryte.svg#icon-trash"></use>
+          </svg>
+      </button>
+
       </a>
     </div>
 
@@ -43,16 +62,52 @@ function generateMovieHTML(bookData) {
 </li> `;
 }
 
-const books = [
-  {
-    image: 'Example',
-    title: 'Example TITLE',
-    genre: 'Example GENRE',
-    description: 'Example DESC',
-    author: 'Example AUTHOR',
-  },
-];
+/////////////приклад масив для тесту, надалі видалити
+// const books = [
+//   {
+//     image: 'Example',
+//     title: 'Example TITLE',
+//     genre: 'Example GENRE',
+//     description: 'Example DESC',
+//     author: 'Example AUTHOR',
+//   },
+// ];
 
-books.forEach(book => {
-  bookList.insertAdjacentHTML('beforeend', generateMovieHTML(book));
-});
+
+
+
+
+
+//////////// додавання книг в локал сторидж, та повернення книг з локалу
+
+// function saveToLocalStorage({ book_image, title, author, description }) {
+//   const cartBooks = JSON.parse(localStorage.getItem(BOOK_ID_INFO)) || [];
+//   cartBooks.push({
+//     book_image:book_image.textContent, 
+//     title, 
+//     author, 
+//     description,
+//   });
+
+//   localStorage.setItem(BOOK_ID_INFO, JSON.stringify(cartBooks));
+
+//   const choosedBooks = JSON.parse(localStorage.getItem(BOOK_ID_INFO)) || [];
+// console.log("choosedBooks", choosedBooks)
+// }
+
+
+// функція для видалення книги при нажиманні смітника
+const shoppingBin = document.querySelector('.btn-png-bin')
+
+shoppingBin.addEventListener('click', onButtonDeletClick);
+function onButtonDeletClick(event) {
+  if (!event.target.classList.contains("btn-png-bin")) {
+    return;
+  } else{
+  const { id } = Number(event.target.closest("класс родителя кнопки").dataset);
+  // const page = pagination.getCurrentPage();
+  const removeIndexFromLocalStorage = choosedBooksArray.findIndex(
+    item => item.id === id
+  );
+  choosedBooksArray.splice(removeIndexFromLocalStorage, 1);
+  localStorage.setItem("ключ", "массив первоначальный");}}
