8aec37265a8853dd577be5f5ccf421dfef3985b8
Kotiko-UA <124360315+Kotiko-UA@users.noreply.github.com>
2023-08-07 20:23:12 +0000

    Deploying to gh-pages from @ Kotiko-UA/team-13-we-can-do-it-JS@0b428f28175eab21ce3618221ba5569d6ef5e15c 🚀

--- assets/shopping-list-b8c344fb.js
+++ assets/shopping-list-b8c344fb.js
@@ -1,4 +1,4 @@
-import{g as tt}from"./theme-cff8195d.js";var Z={exports:{}};/*!
+import{g as tt}from"./theme-cbb42a5d.js";var Z={exports:{}};/*!
  * TOAST UI Pagination
  * @version 3.4.1
  * @author NHN FE Development Team <dl_javascript@nhn.com>
