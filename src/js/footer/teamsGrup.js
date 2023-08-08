72714f0f0e4c62faa4a6d7d752b32f0400499263
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-08-08 23:06:23 +0300

    1

--- src/js/footer/teamsGrup.js
+++ src/js/footer/teamsGrup.js
@@ -1,80 +1,79 @@
 export const teams = [
   {
-    id:'0',
-    img: '../img/footer_img/yevhen-aksonov.jpg',
+    id: '0',
+    img: './img/footer_img/yevhen-aksonov.jpg',
     h: 'Yevhen Aksonov',
     p: 'Team lead',
-    aTeam:'kotiko.ea@gmail.com'
+    aTeam: 'kotiko.ea@gmail.com',
   },
   {
-    id:'1',
-    img: '../img/footer_img/oleksandr-ivashchenko.jpg',
+    id: '1',
+    img: './img/footer_img/oleksandr-ivashchenko.jpg',
     h: 'Ivashchenko Oleksandr',
     p: 'Scrum master',
-    aTeam:'flypyfl@gmail.com'
+    aTeam: 'flypyfl@gmail.com',
   },
   {
-    id:'2',
-    img: '../img/footer_img/ania-kurtiak.jpg',
+    id: '2',
+    img: './img/footer_img/ania-kurtiak.jpg',
     h: 'Ania Kurtiak',
     p: 'Team developer',
-    aTeam:'ania.kiseliuk@gmail.com'
+    aTeam: 'ania.kiseliuk@gmail.com',
   },
 
   {
-    id:'3',
-    img: '../img/footer_img/kate-zaharova.jpg',
+    id: '3',
+    img: './img/footer_img/kate-zaharova.jpg',
     h: 'Kate Zaharova',
     p: 'Team developer',
-    aTeam:'15071981ktz@gmail.com'
+    aTeam: '15071981ktz@gmail.com',
   },
 
   {
-    id:'4',
-    img: '../img/footer_img/artem-khudokonenko.jpg',
+    id: '4',
+    img: './img/footer_img/artem-khudokonenko.jpg',
     h: 'Artem Khudokonenko',
     p: 'Team developer',
-    aTeam:'aftinid94@gmail.com'
+    aTeam: 'aftinid94@gmail.com',
   },
 
   {
-    id:'5',
-    img: '../img/footer_img/lusya-kras.jpg',
+    id: '5',
+    img: './img/footer_img/lusya-kras.jpg',
     h: 'Lusya Kras',
     p: 'Team developer',
-    aTeam:'lusya0409@gmail.com'
+    aTeam: 'lusya0409@gmail.com',
   },
 
   {
-    id:'6',
-    img: '../img/footer_img/igor-volk.jpg',
+    id: '6',
+    img: './img/footer_img/igor-volk.jpg',
     h: 'Igor Volk',
     p: 'Team developer',
-    aTeam:'volkigor286@gmail.com'
+    aTeam: 'volkigor286@gmail.com',
   },
 
   {
-    id:'7',
-    img: '../img/footer_img/viktoriia-sukhara.jpg',
+    id: '7',
+    img: './img/footer_img/viktoriia-sukhara.jpg',
     h: 'Viktoriia Sukhara',
     p: 'Team developer',
-    aTeam:'sukhara.nika13@gmail.com'
+    aTeam: 'sukhara.nika13@gmail.com',
   },
 
   {
-    id:'8',
-    img: '../img/footer_img/oleksander-sharyak.jpg',
+    id: '8',
+    img: './img/footer_img/oleksander-sharyak.jpg',
     h: 'Oleksander Sharyak',
     p: 'Team developer',
-    aTeam:'aleksandrsaryk2001@gmail.com'
+    aTeam: 'aleksandrsaryk2001@gmail.com',
   },
 
-  {id:'9',
-    img: '../img/footer_img/svitlana-trotsenko.jpg',
+  {
+    id: '9',
+    img: './img/footer_img/svitlana-trotsenko.jpg',
     h: 'Svitlana Trotsenko',
     p: 'Team developer',
-    aTeam:'trots.svit@gmail.com'
+    aTeam: 'trots.svit@gmail.com',
   },
-
-
 ];
