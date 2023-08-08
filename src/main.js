c79352d114710ab6ba25e091e729b6fc7bc749ac
Svitlana Trotsenko <trots.svit@gmail.com>
2023-08-08 23:01:10 +0100

    finish1
    add the scroll Top button, fixed paddings on container of mob version, add slow scroll function to the html tag

--- src/main.js
+++ src/main.js
@@ -4,3 +4,4 @@ import './js/footer/footerTeam';
 import './js/heder-js/theme';
 import './js/support-js/support';
 import './js/authorization-js/main_js_authorization';
+import './js/scrollTop/scrollTop';
